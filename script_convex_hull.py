037ca21c363762c95ede674e9ef36c144b43c81e
eariassoto <ariassotoemmanuel@gmail.com>
2016-10-04 11:04:42 -0600

    Mejoras legibilidad codigo, poco mas documentación

--- script_convex_hull.py
+++ script_convex_hull.py
@@ -10,8 +10,9 @@ class snapshot:
         self.id = 0
         self.points = 0
         self.area = 0
-        self.ultimo_peso = 0
+        self.peso = 0
         self.peso_prom = 0
+        self.hull = 0
 
 class interval:
     def __init__(self):
@@ -23,56 +24,69 @@ def get_intervals(inter):
     m = np.loadtxt(open("salida-movimientos.csv","rb"),delimiter=";",skiprows=0)
 
     # busco la cantidad de ranas y el ultimo tic
-    # sumo 1 por el index 0
+    # sumo 1 por el index empieza en 0
     cant_ranas = m[:,1].max() + 1
     max_tic = m[:, 0].max() + 1
 
-    set_m = np.vsplit(m, int(inter))
+    # divido el csv por la cantidad de intervalos
+    # notese que el csv ya está ordenado por tics
+    matrix_by_intervals = np.vsplit(m, int(inter))
 
-    new_set = np.zeros((len(set_m), 1), dtype=object)
+    # vector vacío para devolver los intervalos
+    intervals = np.zeros(len(matrix_by_intervals), dtype=object)
 
     i = 0
-    for m in set_m:
-        abc = m[np.argsort(m[:,1])]
-        abc = np.vsplit(abc, cant_ranas)
+    for m in matrix_by_intervals:
+        # ordeno el intervalo por id de agente
+        interval_by_agents = m[np.argsort(m[:,1])]
+        
+        # divido por la cantidad de agentes
+        interval_by_agents = np.vsplit(interval_by_agents, cant_ranas)
 
-        new_abc = np.zeros((len(abc), 1), dtype=object)
+        # vector vacío para ir procesando agentes de un intervalo
+        agents_interval = np.zeros(len(interval_by_agents), dtype=object)
         j = 0
-        for a in abc:
+        
+        # proceso cada agente en un objeto snapshot
+        for a in interval_by_agents:
             s = snapshot()
             s.interval = i
-            s.id = a[0, 1]
-            s.ultimo_peso = a[-1, 4]
+            s.id = int(a[0, 1])
+            s.peso = a[np.argsort(a[:,0])][-1, 4]
             s.peso_prom = np.average(a[:, 4])
             s.points = a[:, [2,3]]
-            new_abc[j, 0] = s
+            agents_interval[j] = s
             j = j + 1
 
         inter = interval()
         inter.id = i
-        inter.agents = new_abc[:, 0]
-        new_set[i, 0] = inter
+        inter.agents = agents_interval[:]
+        intervals[i] = inter
         i = i + 1    
-    return new_set
+    return intervals
 
 def poly_area(x, y):
     return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
              
 def process_interval(i):
-    for agent in i[0].agents:
-        process_snap(agent)
+    for agent in i.agents:
+        agent = process_snap(agent)
+        print(agent.interval, agent.id, agent.area, agent.peso, agent.peso_prom)
+        #plot_snap(agent)
 
 def process_snap(s):
     points = s.points
     hull = ConvexHull(points)
-    
-    # descomentar para graficar
-    # TODO: hacerlo más fancy
+    s.hull = hull
+    s.area = poly_area(points[hull.vertices, 0], points[hull.vertices, 1])
+    return s
+
+def plot_snap(s):
+    points = s.points
+    hull = s.hull
     plt.plot(points[:,0], points[:,1], 'o')
     for simplex in hull.simplices:
         plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
-    s.area = poly_area(points[hull.vertices, 0], points[hull.vertices, 1])
-    print(s.area)
 
 if __name__ == "__main__":
     inter = sys.argv[1]
@@ -80,7 +94,7 @@ if __name__ == "__main__":
 
     c = 0
     for interval in intervals:
-        plt.figure(c)
+        #plt.figure(c)
         process_interval(interval)
         c = c + 1
-    plt.show()
+    #plt.show()
